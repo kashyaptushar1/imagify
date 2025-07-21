b63fec8e95d7e04939537b44aa3ee94dbce86b10
tushar <tusharkashayp9368@gmail.com>
2025-07-21 15:50:11 +0530

    Header and steps are making in components folder and map into Home.jsx file which is in pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../components/Header'
+import Steps from '../components/Steps'
+
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='' >
+      <Header/>
+      <Steps/>
     </div>
   )
 }
