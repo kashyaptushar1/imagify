b4b26714023bf459bb82c3e93fe97ef35cf27104
tushar <tusharkashayp9368@gmail.com>
2025-07-22 15:00:54 +0530

    Description and Testimonials components are ready and mount to the Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
+import Description from '../components/Description'
+import Testimonials from '../components/Testimonials'
 
 
 const Home = () => {
@@ -8,6 +10,8 @@ const Home = () => {
     <div className='' >
       <Header/>
       <Steps/>
+      <Description/>
+      <Testimonials/>
     </div>
   )
 }
