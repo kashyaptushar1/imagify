a87569b1c49556a5a2a58cf23861b0ea59ffbb59
tushar <tusharkashayp9368@gmail.com>
2025-07-30 23:57:28 +0530

    implement frammer motion and ready frontend

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,7 +5,7 @@ export const AppContext =   createContext();
 
 const AppContextProvider = (props)=>{
     const [user , setUser] = useState(null)
-     const [showLogin , setShowLogin] = useState(true)
+     const [showLogin , setShowLogin] = useState(false)
 
     const value = {
         user , setUser , showLogin , setShowLogin
