aca1eec48efbc92a42f6887411cb70ccc002b903
tushar <tusharkashayp9368@gmail.com>
2025-08-04 23:33:59 +0530

    Add generate image route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import userRouter from './routes/userRoutes.js'
+import imageRouter from './routes/imageRoutes.js'
 
 
 const PORT  = process.env.PORT || 4000
@@ -15,6 +16,7 @@ app.use(cors())
 await connectDB()
 
 app.use('/api/user',userRouter)
+app.use('/api/image',imageRouter)
 
 app.get("/",(req,res)=>{
     res.send("API is working")
